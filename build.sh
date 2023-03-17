eb2dd094f0991cc3b1e32920a2c1070caaae18e9
bit-dream <61420422+bit-dream@users.noreply.github.com>
2023-03-16 22:01:15 -0400

    Updated build.sh so that it outputs unified resume name. Upgraded programming resume content

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ mkdir ./dist/programmer
 mkdir ./dist/controls
 mkdir ./dist/basic
 
-pdflatex -output-directory ./dist/basic derek_black_resume.tex
-pdflatex -output-directory ./dist/controls derek_black_resume_control_focus.tex
-pdflatex -output-directory ./dist/programmer derek_black_resume_program_focus.tex
+pdflatex -output-directory ./dist/basic -jobname derek_black_resume derek_black_resume.tex
+pdflatex -output-directory ./dist/controls -jobname derek_black_resume derek_black_resume_control_focus.tex
+pdflatex -output-directory ./dist/programmer -jobname derek_black_resume derek_black_resume_program_focus.tex
